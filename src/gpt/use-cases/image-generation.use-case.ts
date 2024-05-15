70e6f4d0e7307e31c78f8d2aa360db01924046aa
soRodriguezz <s.rodriguezzapata01@gmail.com>
2024-05-15 14:55:07 -0400

    correccion generate image

--- src/gpt/use-cases/image-generation.use-case.ts
+++ src/gpt/use-cases/image-generation.use-case.ts
@@ -1,6 +1,7 @@
+import * as fs from 'fs';
+
 import OpenAI from 'openai';
 import { downloadBase64ImageAsPng, downloadImagePng } from 'src/helpers';
-import * as fs from 'fs';
 
 interface Options {
   prompt: string;
@@ -14,7 +15,8 @@ export const imageGenerationUseCase = async (
 ) => {
   const { prompt, originalImage, maskImage } = options;
 
-  if (!originalImage || maskImage) {
+  // Todo: verificar original image
+  if (!originalImage || !maskImage) {
     const response = await openai.images.generate({
       prompt: prompt,
       model: 'dall-e-3',
@@ -24,24 +26,27 @@ export const imageGenerationUseCase = async (
       response_format: 'url',
     });
 
+    // Todo: guardar la imagen en FS.
     const fileName = await downloadImagePng(response.data[0].url);
     const url = `${process.env.SERVER_URL}/gpt/image-generation/${fileName}`;
 
     return {
       url: url,
-      openAiUrl: response.data[0].url,
-      revisedPrompt: response.data[0].revised_prompt,
+      openAIUrl: response.data[0].url,
+      revised_prompt: response.data[0].revised_prompt,
     };
   }
 
-  //NOTE - Edición de imagenes
+  // originalImage=http://localhost:3000/gpt/image-generation/1703770602518.png
+  // maskImage=Base64;ASDKJhaskljdasdlfkjhasdkjlHLKJDASKLJdashlkdjAHSKLJDhALSKJD
+
   const pngImagePath = await downloadImagePng(originalImage, true);
   const maskPath = await downloadBase64ImageAsPng(maskImage, true);
 
   const response = await openai.images.edit({
     model: 'dall-e-2',
-    image: fs.createReadStream(pngImagePath),
     prompt: prompt,
+    image: fs.createReadStream(pngImagePath),
     mask: fs.createReadStream(maskPath),
     n: 1,
     size: '1024x1024',
@@ -53,7 +58,7 @@ export const imageGenerationUseCase = async (
 
   return {
     url: url,
-    openAiUrl: response.data[0].url,
-    revisedPrompt: response.data[0].revised_prompt,
+    openAIUrl: response.data[0].url,
+    revised_prompt: response.data[0].revised_prompt,
   };
 };
